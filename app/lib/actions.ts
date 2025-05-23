c8213a0372fea9346a1d87d79a3c6998b093cc2f
Defyan <DefyGit@users.noreply.github.com>
2025-05-23 15:47:09 +0300

    fixed
    fixed the errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,19 +6,20 @@ import { redirect } from 'next/navigation';
 import postgres from 'postgres';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
-import { CreateInvoice, UpdateInvoice } from '../ui/invoices/buttons';
-
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
     invalid_type_error: 'Please select a customer.',
   }),
   amount: z.coerce
-    .number
+    .number()
     .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
-  }), 
+  }),
   date: z.string(),
 });
  
@@ -31,8 +32,11 @@ export type State = {
   message?: string | null;
 };
  
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+ 
 export async function createInvoice(prevState: State, formData: FormData) {
   // Validate form using Zod
+ 
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -70,6 +74,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
   redirect('/dashboard/invoices');
 }
  
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
 export async function updateInvoice(
   id: string,
   prevState: State,
@@ -104,12 +110,12 @@ export async function updateInvoice(
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
+ 
 export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
-
+ 
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
